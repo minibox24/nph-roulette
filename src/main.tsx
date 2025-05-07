2701c5444a396d302697efaaa8ef32237c5c77c3
Minibox <minibox724@gmail.com>
2025-05-08 03:07:31 +0900

    preload

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// `/effect.webm` preload
+// `/effect.webm` video preload
 const video = document.createElement("video");
 video.src = "/effect.webm";
 video.preload = "auto";
@@ -10,5 +10,14 @@ video.muted = true;
 video.autoplay = true;
 video.style.display = "none";
 document.body.appendChild(video);
+
+// `/button.webm` audio preload
+const audio = document.createElement("audio");
+audio.src = "/button.webm";
+audio.preload = "auto";
+audio.muted = true;
+audio.autoplay = true;
+audio.style.display = "none";
+document.body.appendChild(audio);
 
 createRoot(document.getElementById("root")!).render(<App />);
