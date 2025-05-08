959f0354df7bd914d6a4af6597b76cb56cfec81d
Minibox <minibox724@gmail.com>
2025-05-08 17:16:27 +0900

    뽀잉

--- src/components/ResultPage.tsx
+++ src/components/ResultPage.tsx
@@ -179,9 +179,12 @@ function UserCard({ idx, article }: { idx: number; article: BoardArticle }) {
           "_blank",
         );
       }}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.5, delay: TRANSITION_DELAY + 0.1 + idx * 0.1 }}
+      initial={{ opacity: 0, scale: 0.8 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.5,
+        delay: TRANSITION_DELAY + 0.1 + idx * 0.1,
+      }}
     >
       <div
         className="absolute -z-10 size-56 scale-150 rounded-md opacity-70"
